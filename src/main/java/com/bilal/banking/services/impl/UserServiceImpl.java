98e82cc104dc0b19498971008724678683d7402f
Younes <bilal45001@hotmail.com>
2024-06-11 01:25:44 +0200

    implémentation enregistrer utilisateur

--- src/main/java/com/bilal/banking/services/impl/UserServiceImpl.java
+++ src/main/java/com/bilal/banking/services/impl/UserServiceImpl.java
@@ -1,20 +1,26 @@
 package com.bilal.banking.services.impl;
 
 import com.bilal.banking.dto.UserDto;
+import com.bilal.banking.model.User;
 import com.bilal.banking.model.repository.UserRepository;
 import com.bilal.banking.services.UserService;
+import com.bilal.banking.validators.ObjectValidator;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final ObjectValidator<UserDto> validator;
     @Override
     public Integer save(UserDto dto) {
-        return null;
+        validator.validate(dto);
+        User user = UserDto.toEntity(dto);
+        return userRepository.save(user).getId();
     }
 
     @Override
