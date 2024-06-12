4a1c934410b9f562a2743df6e67088d90fa10bc3
Younes <bilal45001@hotmail.com>
2024-06-12 20:15:45 +0200

    implémentation service Account

--- src/main/java/com/bilal/banking/repository/AccountRepository.java
+++ src/main/java/com/bilal/banking/repository/AccountRepository.java
@@ -3,5 +3,8 @@ package com.bilal.banking.repository;
 import com.bilal.banking.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<Account,Integer> {
+    Optional<Account> findByIban(String iban);
 }
--- src/main/java/com/bilal/banking/services/impl/AccountServiceImpl.java
+++ src/main/java/com/bilal/banking/services/impl/AccountServiceImpl.java
@@ -1,15 +1,20 @@
 package com.bilal.banking.services.impl;
 
 import com.bilal.banking.dto.AccountDto;
+import com.bilal.banking.exceptions.OperationNonPermittedException;
 import com.bilal.banking.model.Account;
 import com.bilal.banking.repository.AccountRepository;
 import com.bilal.banking.services.AccountService;
 import com.bilal.banking.validators.ObjectValidator;
-import jakarta.validation.Validator;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.iban4j.CountryCode;
+import org.iban4j.Iban;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class AccountServiceImpl implements AccountService {
@@ -20,24 +25,60 @@ public class AccountServiceImpl implements AccountService {
     @Override
     public Integer save(AccountDto dto) {
 
+        /* if (dto.getId() != null){
+
+            throw new OperationNonPermittedException(
+                    "Account cannot be updated",
+                    "save account",
+                    "Account",
+                    "Update not permitted"
+            );
+        }*/
+
         validator.validate(dto);
         Account account = AccountDto.toEntity(dto);
+
+        if (dto.getId() != null){
+
+            account.setIban(generateRandomIban());
+        }
 
         return repository.save(account).getId();
     }
 
     @Override
     public List<AccountDto> findAll() {
-        return null;
+
+        return repository.findAll()
+                .stream()
+                .map(AccountDto::fromEntity)
+                .collect(Collectors.toList());
     }
 
     @Override
     public AccountDto findById(Integer id) {
-        return null;
+
+        return repository.findById(id)
+                .map(AccountDto::fromEntity)
+                .orElseThrow(() -> new  EntityNotFoundException("No account was found with the ID"+ id));
     }
 
     @Override
     public void delete(Integer id) {
 
+        repository.deleteById(id);
+
+    }
+
+    private String generateRandomIban(){
+
+        String iban = Iban.random(CountryCode.DE).toFormattedString();
+
+        boolean ibanExists = repository.findByIban(iban).isPresent();
+
+        if(ibanExists){
+            generateRandomIban();
+        }
+        return  iban;
     }
 }
