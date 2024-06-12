71594e1ae3b3e54c5d3725ddc6a68a97c55cdaf7
Younes <bilal45001@hotmail.com>
2024-06-12 20:20:32 +0200

    développement service Address

---
+++ src/main/java/com/bilal/banking/services/AddressService.java
@@ -0,0 +1,6 @@
+package com.bilal.banking.services;
+
+import com.bilal.banking.dto.AddressDto;
+
+public interface AddressService extends AbstractService<AddressDto> {
+}
---
+++ src/main/java/com/bilal/banking/services/impl/AddressServiceImpl.java
@@ -0,0 +1,28 @@
+package com.bilal.banking.services.impl;
+
+import com.bilal.banking.dto.AddressDto;
+import com.bilal.banking.services.AddressService;
+
+import java.util.List;
+
+public class AddressServiceImpl implements AddressService {
+    @Override
+    public Integer save(AddressDto dto) {
+        return null;
+    }
+
+    @Override
+    public List<AddressDto> findAll() {
+        return null;
+    }
+
+    @Override
+    public AddressDto findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public void delete(Integer id) {
+
+    }
+}
