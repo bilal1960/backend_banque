beacdbc477dad950e67ecb0e9599d6f2c04fb615
Younes <bilal45001@hotmail.com>
2024-06-15 01:05:46 +0200

    correction mauvaise syntaxe requête database

--- src/main/java/com/bilal/banking/repository/TransactionRepository.java
+++ src/main/java/com/bilal/banking/repository/TransactionRepository.java
@@ -16,7 +16,7 @@ public interface TransactionRepository extends JpaRepository<Transaction,Integer
     List<Transaction> findAllByUserId(Integer userId);
     @Query("select sum (t.amount) from Transaction t where t.user.id = :userId")
     BigDecimal findAccountBalance(@Param("userId") Integer userId);
-    @Query("select max(abs(t.amount)) as amount from Transaction t  where  t.user.id = :userId and t.type = :transactiontype")
+    @Query("select max(abs(t.amount)) as amount from Transaction t  where  t.user.id = :userId and t.type = :transactionType")
     BigDecimal findHighestAmountByTransactionType(Integer userId, TransactionType transactionType);
     @Query("select t.createdDate, sum(t.amount) from  Transaction t where t.user.id = :userId and  t.createdDate between :start and :end group by t.createdDate")
     Map<LocalDate, BigDecimal> findSumTransactionsByDate(LocalDateTime start, LocalDateTime end, Integer userId);
