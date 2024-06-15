d961892775b97e9caab0867d4614deb196169131
Younes <bilal45001@hotmail.com>
2024-06-15 16:36:43 +0200

    développement controller Address

---
+++ src/main/java/com/bilal/banking/controllers/AddressController.java
@@ -0,0 +1,41 @@
+package com.bilal.banking.controllers;
+
+import com.bilal.banking.dto.AddressDto;
+import com.bilal.banking.services.AddressService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/adresses")
+@RequiredArgsConstructor
+public class AddressController {
+
+    private final AddressService service;
+    @PostMapping("/")
+    public ResponseEntity<Integer> save(
+            @RequestBody AddressDto addressDto
+            ){
+             return ResponseEntity.ok(service.save(addressDto));
+            }
+            @GetMapping("/")
+            public ResponseEntity<List<AddressDto>> findAll(){
+                 return ResponseEntity.ok(service.findAll());
+            }
+            @GetMapping("/{address-id}")
+            public ResponseEntity<AddressDto> findById(
+                    @PathVariable("address-id") Integer addressId
+            ){
+                   return ResponseEntity.ok(service.findById(addressId));
+            }
+            @DeleteMapping("/{address-id}")
+            public ResponseEntity<Void> delete(
+                    @PathVariable("address-id") Integer addressId
+
+            ){
+                    service.delete(addressId);
+                    return ResponseEntity.accepted().build();
+            }
+}
