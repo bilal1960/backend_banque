9c5d8ff10bd851408233a60d8bb84685fa78c66b
Younes <bilal45001@hotmail.com>
2024-06-15 16:56:18 +0200

    implémentation controller Account

---
+++ src/main/java/com/bilal/banking/controllers/AccountController.java
@@ -0,0 +1,42 @@
+package com.bilal.banking.controllers;
+
+import com.bilal.banking.dto.AccountDto;
+import com.bilal.banking.services.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/accounts")
+@RequiredArgsConstructor
+public class AccountController {
+
+    private final AccountService service;
+    @PostMapping("/")
+    public ResponseEntity<Integer> save(
+               @RequestBody AccountDto accountDto
+            ){
+               return ResponseEntity.ok(service.save(accountDto));
+    }
+    @GetMapping("/")
+    public ResponseEntity<List<AccountDto>> findAll(){
+               return ResponseEntity.ok(service.findAll());
+    }
+    @GetMapping("/{account-id}")
+    public ResponseEntity<AccountDto> findById(
+            @PathVariable("account-id") Integer accountId
+    ){
+               return  ResponseEntity.ok(service.findById(accountId));
+    }
+
+    @DeleteMapping("/{account-id}")
+    public ResponseEntity<Void> delete(
+            @PathVariable("account-id") Integer accountId
+
+    ){
+        service.delete(accountId);
+        return ResponseEntity.accepted().build();
+    }
+}
--- src/main/java/com/bilal/banking/controllers/AddressController.java
+++ src/main/java/com/bilal/banking/controllers/AddressController.java
@@ -16,26 +16,26 @@ public class AddressController {
     private final AddressService service;
     @PostMapping("/")
     public ResponseEntity<Integer> save(
-            @RequestBody AddressDto addressDto
-            ){
-             return ResponseEntity.ok(service.save(addressDto));
-            }
-            @GetMapping("/")
-            public ResponseEntity<List<AddressDto>> findAll(){
-                 return ResponseEntity.ok(service.findAll());
-            }
-            @GetMapping("/{address-id}")
-            public ResponseEntity<AddressDto> findById(
-                    @PathVariable("address-id") Integer addressId
-            ){
-                   return ResponseEntity.ok(service.findById(addressId));
-            }
-            @DeleteMapping("/{address-id}")
-            public ResponseEntity<Void> delete(
-                    @PathVariable("address-id") Integer addressId
+           @RequestBody AddressDto addressDto
+    ){
+           return ResponseEntity.ok(service.save(addressDto));
+    }
+    @GetMapping("/")
+    public ResponseEntity<List<AddressDto>> findAll(){
+          return ResponseEntity.ok(service.findAll());
+    }
+    @GetMapping("/{address-id}")
+    public ResponseEntity<AddressDto> findById(
+           @PathVariable("address-id") Integer addressId
+    ){
+           return ResponseEntity.ok(service.findById(addressId));
+    }
+    @DeleteMapping("/{address-id}")
+    public ResponseEntity<Void> delete(
+            @PathVariable("address-id") Integer addressId
 
-            ){
-                    service.delete(addressId);
-                    return ResponseEntity.accepted().build();
-            }
+    ){
+            service.delete(addressId);
+            return ResponseEntity.accepted().build();
+    }
 }
